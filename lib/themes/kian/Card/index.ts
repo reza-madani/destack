08824defacbc755bbf3496d7e8594c4c242d0c64
reza.madani <reza.maadani@gmail.com>
2023-09-12 17:36:55 +0330

    feat(theme): add kian test theme

---
+++ lib/themes/kian/Card/index.ts
@@ -0,0 +1,5 @@
+const Component = {
+  displayName: 'Card 1',
+  category: 'Card',
+}
+export default Component
---
+++ lib/themes/kian/index.ts
@@ -0,0 +1,6 @@
+import Card from './Card'
+
+const components = {
+  Card,
+}
+export default components
